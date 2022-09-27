bc81bde6071cad9e8158451a1e5fa8fd955ec7f6
buhuishiwo <1437317989@qq.com>
2022-09-27 17:47:24 +0800

    Site updated: 2022-09-27 17:47:23

--- dist/music.js
+++ dist/music.js
@@ -7,25 +7,13 @@ const ap = new APlayer({
         name: "晴れが来る",
         artist: '春野杉卉',
         url: 'http://m701.music.126.net/20220927180450/7a4808b9d4cf0da924debd0d1da0fac0/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/13503170898/e160/0c76/0cc9/0aea6a08b643418709fdf46509f40876.mp3',
-        cover: 'http://img.ytmp3.cn/image/52.jpg',
+        cover: 'https://p1.music.126.net/WhFprTaP6wZAJ-pFhTyTnw==/109951164594367006.jpg?param=130y130',
       },
       {
-        name: '一百万个可能(Live)',
+        name: '喜劇',
         artist: '摩登兄弟',
-        url: 'http://www.ytmp3.cn/down/52772.mp3',
-        cover: 'http://img.ytmp3.cn/image/53.jpg',
-      },
-      {
-        name: 'The Rose',
-        artist: 'Westlife',
-        url: 'http://www.ytmp3.cn/down/56694.mp3',
-        cover: 'http://img.ytmp3.cn/image/51.jpg',
-      },
-      {
-        name: 'In The Eyes',
-        artist: '江映东',
-        url: 'http://www.ytmp3.cn/down/53053.mp3',
-        cover: 'http://img.ytmp3.cn/image/10.jpg',
+        url: 'http://m801.music.126.net/20220927181017/3f752c50341637bf653baceefc27e57a/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/14468171881/65fe/cbee/4be7/c38ca5bcc6b1faa7370e5a59eb462250.mp3',
+        cover: 'https://p1.music.126.net/_sGZwt82duHeC0et8lsF0w==/109951167257363569.jpg?param=130y130',
       }
     ]
 });
