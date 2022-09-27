1f30ef65c58f4fae59fb02f2228b68d49d134679
buhuishiwo <1437317989@qq.com>
2022-09-27 17:42:39 +0800

    Site updated: 2022-09-27 17:42:38

--- dist/music.js
+++ dist/music.js
@@ -1,17 +1,31 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    autoplay: true,
-    loop: 'all',
-    volume: 0.7,
-    listFolded: true,
-    listMaxHeight: 60,
+    fixed: true,
+    autoplay: false,
     audio: [
-        {
-            name: '前前前世',
-            artist: '',
-            url: 'https://www.ytmp3.cn/down/60420.mp3',
-            cover: 'cover1.jpg',
-        }
-        
+      {
+        name: "晴れが来る",
+        artist: '春野杉卉',
+        url: 'http://m701.music.126.net/20220927180450/7a4808b9d4cf0da924debd0d1da0fac0/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/13503170898/e160/0c76/0cc9/0aea6a08b643418709fdf46509f40876.mp3',
+        cover: 'http://img.ytmp3.cn/image/52.jpg',
+      },
+      {
+        name: '一百万个可能(Live)',
+        artist: '摩登兄弟',
+        url: 'http://www.ytmp3.cn/down/52772.mp3',
+        cover: 'http://img.ytmp3.cn/image/53.jpg',
+      },
+      {
+        name: 'The Rose',
+        artist: 'Westlife',
+        url: 'http://www.ytmp3.cn/down/56694.mp3',
+        cover: 'http://img.ytmp3.cn/image/51.jpg',
+      },
+      {
+        name: 'In The Eyes',
+        artist: '江映东',
+        url: 'http://www.ytmp3.cn/down/53053.mp3',
+        cover: 'http://img.ytmp3.cn/image/10.jpg',
+      }
     ]
 });
